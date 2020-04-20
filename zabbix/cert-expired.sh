3e255125dcaae8caac8f78f91fea90fe4f507c0d
denisitpro <40316333+denisitpro@users.noreply.github.com>
2020-04-20 07:58:31 +0300

    Update cert-expired.sh
    add support port

--- zabbix/cert-expired.sh
+++ zabbix/cert-expired.sh
@@ -2,8 +2,15 @@
 # used for zabbix
 #
 
+SERVER=$1
+PORT=$2
+
+if [ -z "$PORT" ]; then
+  PORT=443
+fi
+
 #!/bin/bash
-end_date=`echo | openssl s_client -connect $1:443 -servername $1 2>/dev/null | openssl x509 -noout -enddate`
+end_date=`echo | openssl s_client -connect $SERVER:$PORT -servername $SERVER 2>/dev/null | openssl x509 -noout -enddate`
 end=$(cut -d= -f2- <<<"$end_date")
 end_date=`date -d "$end" '+%s'`
 curr_date=`date '+%s'`
